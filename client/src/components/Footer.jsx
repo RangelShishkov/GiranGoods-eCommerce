492dab901257273ae882d80d02d6506db8ed8f66
Rangel Shishkov <93160824+RangelShishkov@users.noreply.github.com>
2024-08-08 17:15:32 +0300

    create Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     return(
-        <div>Footer</div>
+        <footer className="bg-slate-200">
+            <div className="flex justify-center gap-2 mx-auto p-4">
+                <div className="text-2xl">
+                <FaGithub/>
+                </div>
+                <div>
+                <a href="https://github.com/RangelShishkov"><p className="font-bold">github.com/RangelShishkov</p></a> 
+                </div>
+            </div>
+        </footer>
     )
 }
 
